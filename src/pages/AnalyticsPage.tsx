943e50205d822d1e58ecdc6b12de750911839c9a
commaeng09 <41318974+commaeng09@users.noreply.github.com>
2025-07-28 23:10:24 +0900

    Fix: Remove unused Line import to resolve build error

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -14,7 +14,7 @@ import {
   PointElement,
   LineElement,
 } from 'chart.js';
-import { Bar, Pie, Line } from 'react-chartjs-2';
+import { Bar, Pie } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
